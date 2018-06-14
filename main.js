b72093b6ad8bb0c980bfb45dd10c0c70e87856bc
Alice Fabre <alicefabre@MacBook-Pro-de-Alice-2.local>
2018-06-14 12:50:53 +0200

    Fixes#1

--- main.js
+++ main.js
@@ -41,3 +41,20 @@ function upAction() {
 window.addEventListener("scroll", function() {
   scrollDetect(homeAction, downAction, upAction);
 });
+
+
+
+var burgerMenu = document.querySelector('.menu');
+var imgClose = document.querySelector('.menu-close');
+var mainnav = document.getElementById('mainav');
+
+
+burgerMenu.addEventListener('click', function() {
+  mainav.style.display = 'block';
+  imgClose.style.display = 'block';
+
+})
+imgClose.addEventListener('click', function() {
+  mainav.style.display = 'none';
+  imgClose.style.display = 'none';
+})
